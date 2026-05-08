5e3afd899fa1f02227bda98552f2dbf34165ec6b
Michael <michaelkindie14@gmail.com>
2026-05-08 11:50:43 -0500

    feat: implemented a tasks service to handle  adding new tasks and updating task status, modified corresponding files to implement feature

--- angular_services/src/app/tasks/tasks-list/tasks-list.component.ts
+++ angular_services/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,8 +11,16 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private TasksService = inject(TasksService);
+  private selectedFilter = signal<string>('all');
+  tasks = computed(() => { 
+    switch (this.selectedFilter()) {
+      case 'open': return this.TasksService.allTasks().filter(task => task.status === 'OPEN');
+      case 'in-progress': return this.TasksService.allTasks().filter(task => task.status === 'IN_PROGRESS');
+      case 'done': return this.TasksService.allTasks().filter(task => task.status === 'DONE');
+      default: return this.TasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
